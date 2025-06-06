e6bbb8b44edd929d889ea912f09eb3e57824e384
anni990 <aniket22mishra2004@gmail.com>
2025-06-06 20:03:29 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   plugins: [react()],
   base: "/codestrom-hackathon/",
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src'),
-    },
-  },
-}));
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+      }
+    }
+  }
+});
