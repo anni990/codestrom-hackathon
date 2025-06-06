6aba4811c593ca9f717e02bd97e5e26aa31f83ff
anni990 <aniket22mishra2004@gmail.com>
2025-06-06 19:50:28 +0530

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <BrowserRouter basename="/codestrom-hackathon/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
