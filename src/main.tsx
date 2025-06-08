2063d78f9014833519594048719a731324b8a3fa
anni990 <aniket22mishra2004@gmail.com>
2025-06-08 23:22:34 +0530

    ....

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import './index.css';
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter basename="/codestrom-hackathon/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
